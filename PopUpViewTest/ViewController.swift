d12c109f21d89c7046079c611a868f1d76b9df42
Baseline <drynn@apl406c8f19ac34.doitt.nycnet>
2016-03-29 12:11:25 -0400

    Initial Commit

---
+++ PopUpViewTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PopUpViewTest
+//
+//  Created by Rynn, David on 3/29/16.
+//  Copyright © 2016 Rynn, David. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
